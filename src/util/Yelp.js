e382e64b4d9075740f7c081709e0d381a2fee40b
iharmanpannu <iharmanpannu@gmail.com>
2017-12-25 11:27:23 -0800

    added yelp

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,12 +1,8 @@
 const apiKey = 'WfwS3ga6nHNgONSp7xYNHn4SPersWkm1Alf0Rx9Lb1ffP8ysm_HHIsEV8tRNyfYLx0dD0RurIb7F5y4P' +
     'sOK5fGDWJ6CMg7FKKeJws4wA4oxWCPXpNqPrdqYFXbU4WnYx';
 
-
 const Yelp = {};
 
-function search(term, location, sortBy) {
-    return fetch(
-        `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-        const header = `Bearer ${WfwS3ga6nHNgONSp7xYNHn4SPersWkm1Alf0Rx9Lb1ffP8ysm_HHIsEV8tRNyfYLx0dD0RurIb7F5y4PsOK5fGDWJ6CMg7FKKeJws4wA4oxWCPXpNqPrdqYFXbU4WnYx}`
-    );
+search = (term, location, sortBy) => {
+  return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`)
 }
